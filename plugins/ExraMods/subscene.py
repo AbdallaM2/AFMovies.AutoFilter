4258ef14ab6ca5f05ab9258b65f17daf1ecef2e3
Abdalla2307 <99101601+Abdalla2307@users.noreply.github.com>
2022-12-04 04:29:14 +0200

    Update subscene.py

--- plugins/ExraMods/subscene.py
+++ plugins/ExraMods/subscene.py
@@ -1,75 +1,129 @@
-import requests,urllib
-import os,sys,re,zipfile,shutil,io
 from bs4 import BeautifulSoup
-cwd = os.getcwd()
+from .base import Base
+import re
 
 
-# taking the movie input 
+class Subscene(Base):
+@Client.on_message(filters.command(["sub]))
+    async def search(self, title):
+        """
+        search function gives a title and search for it in subscene
+        results will be passed as list of dicts, sample:
+        {"name": "movie/series title", "link": "link to that title", "count": "counts"}
+        """
+        try:
+            url = f"https://subscene.com/subtitles/searchbytitle?query={title}"
+            resp = await self.aiorequest(url=url)  # send request to Subscene
+            if " " in title:
+                title = title.replace(" ", "+")
+            soup = BeautifulSoup(resp, 'lxml')
+            find_ul = soup.find('div', class_='search-result').find_all('ul')  # .find('ul')
 
-@Client.on_message(filters.command(["Sub"]))
-async def subscene(client, message):
-movie_name = [s for s in re.split("[^0-9a-zA-Z]",input("enter the movie name : \n"))]
-movie_name = list(filter(lambda a: a != '', movie_name))
-m1 = ' '.join(map(str,movie_name))
+            subtitles = []  # list for all finded subs to return
+            for ul in find_ul:
+                find_li = ul.find_all('li')
+                for li in find_li:
+                    base_li = li.find('div', class_='title').a
+                    name = base_li.text  # movie/show name
+                    link = base_li['href']  # movie/show url
+                    try:  # movie/show subtitle counts
+                        sub_count = li.find('div', class_='subtle count').text
+                    except:
+                        sub_count = li.find('span', class_='subtle count').text
+                    sub_count = re.findall(r'\d+', sub_count)[0]
 
-encodings = []
-while len(encodings) == 0:
-	encodings = [s.lower() for s in re.split("[^0-9a-zA-Z]",input("enter the storage format (eg.720p,bluray,brrip,xvid,hdtv etc) (must) \n"))]
-	if len(encodings) == 0 :
-		print("You must enter some encoding format")
-encodings = list(filter(lambda a: a != '', encodings))
-m2 = ' '.join(map(str,encodings))
-m1 = m1 + ' ' + m2
+                    link = "https://subscene.com" + link
 
-print("you have searched for \n",m1)
+                    data = {"name": name, "link": link, "count": int(sub_count)}
+                    subtitles.append(data)
 
+            return subtitles
+        except Exception as e:
+            print(e)
+            return []
 
-search_string = m1.split()
-#search_string
+    async def subtitles(self, url, lang=None):
+        try:
+            resp = await self.aiorequest(url, lang)
+            soup = BeautifulSoup(resp, 'lxml')
+            title = soup.find('div', class_='box clearfix').find('div', class_='top left').find('div',
+                                                                                                class_='header').h2.text
+            try:
+                title = title.replace("Flag", "")
+                title = title.replace("Imdb", "").strip()
+            except:
+                pass
 
+            table = soup.table.tbody.find_all('tr')
 
-''' Preparing the query '''
+            subtitles = []
+            for tr in table:
+                try:
+                    sub_name = tr.find('td', class_='a1').a.find_all('span')[1].text.strip()  # release title
+                except AttributeError:
+                    continue
+                sub_link = tr.find('td', class_='a1').a['href']  # release link
+                try:
+                    sub_owner = tr.find('td', class_='a5').a.text.strip()  # sub owner
+                except:
+                    sub_owner = "Anonymous"
+                try:
+                    comments = tr.find('td', class_='a6').text.strip()
+                except:
+                    comments = ""
+                sub_link = "https://subscene.com" + sub_link
 
-search_url = "https://subscene.com/subtitles/title?q="
-search_url += search_string[0]
-for words in search_string[1:]:
-    search_url += ("+" +  words)
-search_url += "&l="    
-print(search_url)
+                sub = {"name": sub_name, "link": sub_link, "owner": sub_owner, "comments": comments}
+                subtitles.append(sub)
 
+            re_subtitles = {"title": title, "subtitles": subtitles}
+            return re_subtitles
+        except Exception as e:
+            print(e)
+            return []
 
-r = requests.get(search_url)
-soup = BeautifulSoup(r.content,"lxml")
-#print(soup)
+    async def down_page(self, url):
+        resp = await self.aiorequest(url)
+        soup = BeautifulSoup(resp, 'lxml')
 
-subs = soup.find_all("td", class_ = "a1") 
-#print(subs)
+        maindiv = soup.body.find('div', class_='subtitle').find('div', class_='top left')
+        title = maindiv.find('div', class_='header').h1.span.text.strip()
+        try:
+            imdb = maindiv.find('div', class_='header').h1.a['href']
+        except TypeError:
+            imdb = ""
+        try:
+            poster = maindiv.find('div', class_='poster').a['href']
+        except:
+            poster = ""
+        try:
+            author_name = maindiv.find('div', class_='header').ul.find('li', class_='author').a.text.strip()
+            author_link = f"https://subscene.com{maindiv.find('div', class_='header').ul.find('li', class_='author').a['href']}"
+        except:
+            author_link = ""
+            author_name = "Anonymous"
 
+        download_url = f"https://subscene.com{maindiv.find('div', class_='header').ul.find('li', class_='clearfix').find('div', class_='download').a['href']}"
 
-for elements in range(len(subs)) :
-    res = subs[elements].find_all("span", class_="l r positive-icon")
-    s = str(res)
-    m = re.search('English',s)
-    if m :
-        target = subs[elements]
-        t = target.find("a")
-        download_link = t['href']
-        break
-        
+        try:
+            comments = maindiv.find('div', class_='header').ul.find('li', class_='comment-wrapper').find('div',
+                                                                                                         class_='comment').text
+        except:
+            comments = ""
+        try:
+            release = maindiv.find('div', class_='header').ul.find('li', class_='release').find_all('div')
+            releases = ""
+            for i in range(2):
+                r = release[i].text.strip()
+                releases = releases + f"\n{r}"
+        except Exception as e:
+            releases = ""
 
-# download that link
-r1 = requests.get("https://subscene.com" + download_link)
-soup = BeautifulSoup(r1.content,"lxml")
-download = soup.find_all('a',attrs={'id':'downloadButton'})[0].get("href")
-#print(download)
-r2 = requests.get("http://subscene.com" + download)
-download_link = r2.url
-#print(r2.encoding)
-#print(file_path) 
+        response = {"title": title, "imdb": imdb, "poster": poster, "author_name": author_name,
+                    "author_url": author_link, "download_url": download_url, "comments": comments, "releases": releases}
+        return response
 
+    async def download(self, url, file_path):
+        resp = await self.download_file(url, file_path)
 
-f = requests.get(download_link)
-zipped = zipfile.ZipFile(io.BytesIO(f.content))
-zipped.extractall()
-
-print("subtitles downloaded succesfully")
+        return resp
