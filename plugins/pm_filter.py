8a510a3996fca4546fffc6d59876d2958cd95d24
AbdallaM01 <100961487+AbdallaM01@users.noreply.github.com>
2023-01-03 15:59:39 +0200

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -173,7 +173,7 @@ async def pm_next_page(bot, query):
 async def next_page(bot, query):
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer("⚠️ 𝙏𝙝𝙖𝙩'𝙨 𝙉𝙤𝙩 𝙁𝙤𝙧 𝙔𝙤𝙪. 𝙋𝙡𝙚𝙖𝙨𝙚 𝙍𝙚𝙦𝙪𝙚𝙨𝙩 𝙔𝙤𝙪𝙧 𝙊𝙬𝙣\n(عذرا! من فضلك ارسل طلبك الخاص ⚠️)", show_alert=True)
+        return await query.answer("⚠️ 𝙏𝙝𝙖𝙩'𝙨 𝙉𝙤𝙩 𝙁𝙤𝙧 𝙔𝙤𝙪. 𝙋𝙡𝙚𝙖𝙨𝙚 𝙍𝙚𝙦𝙪𝙚𝙨𝙩 𝙔𝙤𝙪𝙧 𝙊𝙬𝙣\n(⚠️ عذرا! من فضلك ارسل طلبك الخاص)", show_alert=True)
     try:
         offset = int(offset)
     except:
