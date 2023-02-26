ae63f5b7a34f2ba3e4d3cdab507bb78fe8b5d730
Abdalla2307 <99101601+Abdalla2307@users.noreply.github.com>
2023-02-26 05:19:25 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -365,3 +365,22 @@ def humanbytes(size):
     return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
 
 
+async def get_shortlink(link):
+
+    url = f'{SHORT_URL}/api'
+    params = {
+      'api': SHORT_API,
+      'url': link,
+    }
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
+                data = await response.json()
+                if data["status"] == "success":
+                    return data['shortenedUrl']
+                else:
+                    logger.error(f"Error: {data['message']}")
+                    return link
+    except Exception as e:
+        logger.error(e)
+        return link
