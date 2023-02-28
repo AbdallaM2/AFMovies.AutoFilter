6a9a9856f74d82dc137dc8a61910435027f5fb28
Abdalla2307 <99101601+Abdalla2307@users.noreply.github.com>
2023-02-28 03:21:58 +0200

    Update next_and_spell.py

--- plugins/next_and_spell.py
+++ plugins/next_and_spell.py
@@ -57,7 +57,16 @@ async def next_page(bot, query):
             btn = [[InlineKeyboardButton(text=f"[{get_size(file.file_size)}] {file.file_name}", callback_data=f'files#{nxreq}#{file.file_id}')] for file in files ]
         else:
             btn = [[InlineKeyboardButton(text=f"{file.file_name}", callback_data=f'files#{nxreq}#{file.file_id}'),
-                    InlineKeyboardButton(text=f"{get_size(file.file_size)}", callback_data=f'files#{nxreq}#{file.file_id}')] for file in files ]
+                    InlineKeyboardButton(text=f"{get_size(file.file_size)}", callback_data=f'files#{nxreq}#{file.file_id}',
+                ),
+            ]
+            for file in files
+        ]
+    btn.insert(10,
+        [
+            InlineKeyboardButton(text="⚡𝐂𝐡𝐞𝐜𝐤 𝐏𝐌⚡", url='t.me/ArrowFlixBot')
+        ]
+    )
 
     if 0 < offset <= 10:
         off_set = 0
@@ -198,19 +207,4 @@ async def pm_spoll_tester(bot, query):
         k = await query.message.edit('This Movie Not Found In DataBase')
         await asyncio.sleep(10)
         await k.delete()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
