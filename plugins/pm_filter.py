e1a0bc97b0023aa9dbaa6114948f0dc5fcf2b269
Abdalla2307 <99101601+Abdalla2307@users.noreply.github.com>
2023-03-01 15:49:07 +0200

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -60,9 +60,8 @@ async def pm_AutoFilter(client, msg, pmspoll=False):
                     InlineKeyboardButton(text=f"{get_size(file.file_size)}", callback_data=f'{pre}#{file.file_id}')] for file in files ]    
     btn.insert(0,
         [
-                InlineKeyboardButton(f'sᴇʀɪᴇs', 'ɪɴꜰᴏ'),
-                InlineKeyboardButton(f'ᴍᴏᴠɪᴇs', 'ᴍᴏᴠɪᴇs'),
-                InlineKeyboardButton(f'ɪɴꜰᴏ', 'sᴇʀɪᴇs')
+                InlineKeyboardButton(f'ᴍᴏᴠɪᴇs', 'ɪɴꜰᴏ'),
+                InlineKeyboardButton(f'ɪɴꜰᴏ', 'ᴍᴏᴠɪᴇs')
         ]
     )
     btn.insert(11,
