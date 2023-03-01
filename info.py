53814190edf077ef01815e3239964d438199ff87
Abdalla2307 <99101601+Abdalla2307@users.noreply.github.com>
2023-03-01 05:24:53 +0200

    Update info.py

--- info.py
+++ info.py
@@ -62,6 +62,7 @@ SHORT_URL = environ.get("SHORT_URL")
 SHORT_API = environ.get("SHORT_API")
 
 # Others
+IMDB_DELET_TIME = int(environ.get('IMDB_DELET_TIME', "300"))
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'ArrowFlix Discussion | Series & Movies')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), True)
