a9860bd3f53f180f7a9bf0a923cc0aec044a0cc9
Abdalla2307 <99101601+Abdalla2307@users.noreply.github.com>
2023-03-01 14:56:01 +0200

    Update group_filter.py

--- plugins/group_filter.py
+++ plugins/group_filter.py
@@ -134,6 +134,7 @@ async def auto_filter(client, msg, spoll=False):
     btn.insert(0,
         [
                 InlineKeyboardButton(f'sᴇʀɪᴇs', 'ɪɴꜰᴏ'),
+                InlineKeyboardButton(f'ᴍᴏᴠɪᴇs', 'ᴍᴏᴠɪᴇs'),
                 InlineKeyboardButton(f'ɪɴꜰᴏ', 'sᴇʀɪᴇs')
         ]
     )
