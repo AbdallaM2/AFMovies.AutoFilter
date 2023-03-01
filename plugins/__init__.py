6cc4cde6ee8c7caae5dc6898b882d5d831dd1bc5
Abdalla2307 <99101601+Abdalla2307@users.noreply.github.com>
2023-03-01 19:56:07 +0200

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -10,8 +10,3 @@ async def web_server():
     web_app = web.Application(client_max_size=30000000)
     web_app.add_routes(routes)
     return web_app
-
-
-__repo__ = "https://github.com/AbdallaM01/autofilter.extra"
-__license__ = "GNU GENERAL PUBLIC LICENSE V2"
-__copyright__ = "Copyright (C) 2023-present Abdalla <https://github.com/AbdallaM01>"
