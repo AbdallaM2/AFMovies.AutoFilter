ce4da60615408298a13c2356c4214e7be37ecf59
Abdalla2307 <99101601+Abdalla2307@users.noreply.github.com>
2023-03-01 14:00:39 +0200

    Update next_and_spell.py

--- plugins/next_and_spell.py
+++ plugins/next_and_spell.py
@@ -58,6 +58,11 @@ async def next_page(bot, query):
         else:
             btn = [[InlineKeyboardButton(text=f"{file.file_name}", callback_data=f'files#{nxreq}#{file.file_id}'),
                     InlineKeyboardButton(text=f"{get_size(file.file_size)}", callback_data=f'files#{nxreq}#{file.file_id}')] for file in files ]
+    btn.insert(10,
+        [
+            InlineKeyboardButton(text="⚡𝐂𝐡𝐞𝐜𝐤 𝐏𝐌⚡", url='t.me/ArrowFlixBot')
+        ]
+    )
 
     if 0 < offset <= 10:
         off_set = 0
@@ -148,6 +153,11 @@ async def pm_next_page(bot, query):
         else:
             btn = [[InlineKeyboardButton(text=f"{file.file_name}", callback_data=f'pmfile#{file.file_id}'),
                     InlineKeyboardButton(text=f"{get_size(file.file_size)}", callback_data=f'pmfile#{file.file_id}')] for file in files ]
+    btn.insert(10,
+        [
+            InlineKeyboardButton(text="⚡𝐂𝐡𝐞𝐜𝐤 𝐏𝐌⚡", url='t.me/ArrowFlixBot')
+        ]
+    )
 
     if 0 < offset <= 10:
         off_set = 0
