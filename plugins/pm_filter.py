a94e5c1e28352e6a1692c4f6ddc1e59b31fca69b
AbdallaM2 <119325558+AbdallaM2@users.noreply.github.com>
2023-03-06 15:47:12 +0200

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -64,6 +64,11 @@ async def pm_AutoFilter(client, msg, pmspoll=False):
                 InlineKeyboardButton(f'ɪɴꜰᴏ', 'ᴍᴏᴠɪᴇs')
         ]
     )
+btn.insert(11,
+        [
+            InlineKeyboardButton(text="⚡Shorten Link & تخطي الرابط⚡", url='https://t.me/ArrowFlix2Bot?start=ZmlsZV9CQUFEQkFBRGdROEFBazV5TVZEbFVyT1dtUTVBN0JZRQ')
+        ]
+    )
 
     if offset != "":
         key = f"{message.chat.id}-{message.id}"
