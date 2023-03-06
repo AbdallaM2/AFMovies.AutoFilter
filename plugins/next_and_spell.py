3aefa2584fecbb08319e14712a37f9d5d2647580
AbdallaM2 <119325558+AbdallaM2@users.noreply.github.com>
2023-03-06 16:57:32 +0200

    Update next_and_spell.py

--- plugins/next_and_spell.py
+++ plugins/next_and_spell.py
@@ -26,7 +26,7 @@ logger.setLevel(logging.ERROR)
 async def next_page(bot, query):
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer("oKda", show_alert=True)
+        return await query.answer("⚠️ 𝙏𝙝𝙖𝙩'𝙨 𝙉𝙤𝙩 𝙁𝙤𝙧 𝙔𝙤𝙪. 𝙋𝙡𝙚𝙖𝙨𝙚 𝙍𝙚𝙦𝙪𝙚𝙨𝙩 𝙔𝙤𝙪𝙧 𝙊𝙬𝙣\n(عذرا! من فضلك ارسل طلبك الخاص ⚠️)", show_alert=True)
     try:
         offset = int(offset)
     except:
@@ -106,7 +106,7 @@ async def next_page(bot, query):
 async def advantage_spoll_choker(bot, query):
     _, user, movie_ = query.data.split('#')
     if int(user) != 0 and query.from_user.id != int(user):
-        return await query.answer("okDa", show_alert=True)
+        return await query.answer("⚠️ 𝙏𝙝𝙖𝙩'𝙨 𝙉𝙤𝙩 𝙁𝙤𝙧 𝙔𝙤𝙪. 𝙋𝙡𝙚𝙖𝙨𝙚 𝙍𝙚𝙦𝙪𝙚𝙨𝙩 𝙔𝙤𝙪𝙧 𝙊𝙬𝙣\n(عذرا! من فضلك ارسل طلبك الخاص ⚠️)", show_alert=True)
     if movie_ == "close_spellcheck":
         return await query.message.delete()
     movies = temp.SPELL_CHECK.get(query.message.reply_to_message.id)
